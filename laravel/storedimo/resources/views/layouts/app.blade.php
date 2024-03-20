5c3a8ec291bb21bc07c8b435d8c9ed37379b4644
Juangui <jgmejiaco@gmail.com>
2024-03-20 09:06:50 -0500

    implementacion de librerias complementarias de laravel para la sintaxis de migraciones y formularios, ademas del link storage

---
+++ laravel/storedimo/resources/views/layouts/app.blade.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        <meta name="description" content="">
+        <meta name="keywords" content="">
+        <meta name="author" content="">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>Panel - @yield('title')</title>
+        @yield('css')
+
+        {{-- Favicon --}}
+        {{-- <link rel="shortcut icon" href="{{asset('imagenes/logo_jgmcdigital.png')}}" type="image/x-icon"> --}}
+
+        {{-- ========================================= --}}
+
+        <!-- Bootstrap CSS 5.3.2 -->
+        {{-- <link rel="stylesheet" href="{{asset('bootstrap/bootstrap.5.3.2.min.css')}}" > --}}
+
+        {{-- ========================================= --}}
+        
+        <!--  Js -->
+        {{-- <script src="{{asset('js/modernizr.custom.js')}}"></script> --}}
+        {{-- <script src="{{asset('js/jquery-3.6.0.min.js')}}"></script> --}}
+
+        {{-- ========================================= --}}
+
+        {{-- SELECT2 --}}
+        {{-- <link rel="stylesheet" href="{{asset('vendor/select2-4.1.0/dist/css/select2.min.css')}}"> --}}
+
+        {{-- ========================================= --}}
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{asset('font-awesome-4.5.0/css/font-awesome.min.css')}}"> Necesario para el ícono del logout
+        {{-- <link rel="stylesheet" href="{{asset('css/style.css')}}"> --}}
+
+
+        {{-- ========================================= --}}
+
+        {{-- Sweetalert2 (No necesita jquery para funcionar) --}}
+        <link rel="stylesheet" type="text/css" href="{{asset('css/sweetalert2.css')}}">
+        <link rel="stylesheet" type="text/css" href="{{asset('css/sweetalert2.min.css')}}">
+    </head>
+
+    {{-- =========================================================================== --}}
+    {{-- =========================================================================== --}}
+
+    <body>
+        <div class="container-fluid p-0 position-relative">
+            <div class="">
+                {{-- @if(Request()->path() == '/' || Request()->path() == "login" || Request()->path() == "logout")
+                    @include('layouts.topbar_login')
+                @elseif(Request()->path() == "recuperar")
+                    @include('layouts.topbar_login')
+                @else
+                    @include('layouts.topbar')
+                @endif --}}
+
+                {{-- ===================================== --}}
+
+                <div class="container-fluid p-3">
+                    <div class="content">
+                        @yield('content')
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        {{-- @include('layouts.footer') --}}
+
+        {{-- ======================================================== --}}
+        {{-- ======================================================== --}}
+
+        @yield('scripts')
+
+        <!-- Bootstrap Bundle JS 5.3.2 -->
+        {{-- <script src="{{asset('bootstrap/bootstrap5.3.2.bundle.min.js')}}"></script> --}}
+
+        {{-- ========================================================================= --}}
+
+        <!-- SELECT2 -->
+        {{-- <script src="{{asset('vendor/select2-4.1.0/dist/js/select2.min.js')}}"></script> --}}
+
+        {{-- ========================================================================= --}}
+
+        {{-- Sweetalert (No necesita jquery para funcionar) --}}
+        {{-- <script src="{{asset('js/sweetalert2.all.js')}}"></script> --}}
+        {{-- <script src="{{asset('js/sweetalert2.min.js')}}"></script> --}}
+        
+        <!-- SCRIPTS -->
+        @include('sweetalert::alert')
+    </body>
+</html>
