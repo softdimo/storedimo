451b43681edb8795f37eb557581829b84f59c89e
Juangui <jgmejiaco@gmail.com>
2024-03-20 11:23:51 -0500

    estilos  inicio de sesion

--- laravel/storedimo/resources/views/inicio_sesion/login.blade.php
+++ laravel/storedimo/resources/views/inicio_sesion/login.blade.php
@@ -15,40 +15,44 @@
 
 @section('content')
     <div class="vh-100 d-flex flex-column align-items-center justify-content-center">
-        <img src="{{asset('imagenes/logo.png')}}" alt="">
-
-        {{-- =========================================================== --}}
-
-        <form class="" method="post" action="" autocomplete="off">
-            @csrf
-            
-            {{-- ============================ --}}
-
-            <div class="mb-4">
-                <input class="w-100 form-control" type="text" name="usuario" id="usuario" placeholder="Nombre de Usuario *">
+        <div class="w-25 border border-1">
+            <div class="d-flex justify-content-center p-1" style="background-color:#F5F5F5">
+                <img src="{{asset('imagenes/logo.png')}}" alt="logo" class="text-center" width="200" height="100">
             </div>
 
-            {{-- ============================ --}}
+            {{-- =========================================================== --}}
 
-            <div class="">
-                <span class="btn-show-pass">
-                    <i class="zmdi zmdi-eye"></i>
-                </span>
-                <input class="w-100 form-control" type="password" name="clave" id="clave" placeholder="Contraseña *">
-            </div>
+            <form class="bg-white p-3 mt-3" method="post" action="" autocomplete="off">
+                @csrf
+                
+                {{-- ============================ --}}
 
-            {{-- ============================ --}}
+                <div class="mb-4">
+                    <input class="w-100 form-control" type="text" name="usuario" id="usuario" placeholder="Nombre de Usuario *">
+                </div>
 
-            <div class="mt-5 d-flex justify-content-center">
-                <button class="btn btn-primary" type="submit">Iniciar Sesión</button>
-            </div>
+                {{-- ============================ --}}
 
-            {{-- ============================ --}}
+                <div class="">
+                    <span class="btn-show-pass">
+                        <i class="zmdi zmdi-eye"></i>
+                    </span>
+                    <input class="w-100 form-control" type="password" name="clave" id="clave" placeholder="Contraseña *">
+                </div>
 
-            <div class="mt-5 d-flex justify-content-end">
-                <a class="" href="" style="color: blue">Olvidé mi Clave</a>
-            </div>
-        </form>
+                {{-- ============================ --}}
+
+                <div class="mt-5 d-flex justify-content-center">
+                    <button class="btn btn-primary" type="submit">Iniciar Sesión</button>
+                </div>
+
+                {{-- ============================ --}}
+
+                <div class="mt-5 d-flex justify-content-end">
+                    <a class="" href="" style="color: blue">Olvidé mi Clave</a>
+                </div>
+            </form>
+        </div>
     </div>
 @stop
 
