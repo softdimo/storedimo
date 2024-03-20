f87a7bc9b1d146148fc4d3a37b9246c0f2050fba
Juangui <jgmejiaco@gmail.com>
2024-03-20 16:25:52 -0500

    implementacion del home

---
+++ laravel/storedimo/app/Http/Controllers/home/HomeController.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Http\Controllers\home;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Exception;
+use App\Http\Controllers\admin\AdministradorController;
+use App\Models\Usuario;
+
+class HomeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // try {
+            // $adminCtrl = new AdministradorController();
+            // $sesion = $adminCtrl->validarVariablesSesion();
+
+            // if (empty($sesion[0]) || is_null($sesion[0]) &&
+            //     empty($sesion[1]) || is_null($sesion[1]) &&
+            //     empty($sesion[2]) || is_null($sesion[2]) && !$sesion[3])
+            // {
+            //     return view('inicio_sesion.login');
+            // } else {
+            //     $usuLogueado = session('id_usuario');
+                // $usuario = Usuario::select('nombres')->where('id_usuario',$usuLogueado)->first();
+                return view('home.index');
+        //     }
+        // } catch (Exception $e) {
+        //     alert()->error("Error Exception!");
+        //     return redirect()->to(route('login'));
+        // }
+    }
+
+    // ======================================================================
+    // ======================================================================
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        
+    }
+
+    // ======================================================================
+    // ======================================================================
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        
+    }
+
+    // ======================================================================
+    // ======================================================================
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    // ======================================================================
+    // ======================================================================
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+    
+    // ======================================================================
+    // ======================================================================
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    // ======================================================================
+    // ======================================================================
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
