21252b0ed48670b732078a88149ef8533cd541bc
Juangui <jgmejiaco@gmail.com>
2024-03-26 11:53:15 -0500

    estilos side bar menu

--- laravel/storedimo/resources/views/layouts/sidebarmenu.blade.php
+++ laravel/storedimo/resources/views/layouts/sidebarmenu.blade.php
@@ -1,14 +1,50 @@
-<aside class="left-sidebar">
-    <nav class="sidebar-nav">
-        <ul id="sidebarnav">
-            <li>Menú Principal</li>
-            <li>Personas</li>
-            <li>Categorías</li>
-            <li>Gestionar Existencias</li>
-            <li>Entradas</li>
-            <li>Ventas</li>
-            <li>Préstamos a Empleados</li>
-            <li>Pagos a Empleados</li>
+<aside class="">
+    <nav class="navbar sidebar-nav navbar-collapse" role="navigation">
+        <ul class="nav d-flex flex-column" id="sidebarnav">
+            <li>
+                <i class="fa fa-th-list col-1" style="color: #000"></i>
+                <a href="" class="active text-decoration-none col-9">Menú Principal</a>
+            </li>
+            <li>
+                <i class="fa fa-users col-1" style="color: #000"></i>
+                <a href="" class="text-decoration-none col-9">Personas</a>
+                <span class="fa arrow" style="color: red"></span>
+            </li>
+            <li>
+                <i class="fa fa-database col-1" style="color: #000"></i>
+                <a href="" class="text-decoration-none col-9">Categorías</a>
+                <span class="fa arrow" style="color: #000"></span>
+            </li>
+            <li>
+                <i class="fa fa-cubes col-1" style="color: #000"></i>
+                <a href="" class="text-decoration-none col-9">Productos</a>
+                <span class="fa arrow" style="color: #000"></span>
+            </li>
+            <li>
+                <i class="fa fa-check-square-o col-1" style="color: #000"></i>
+                <a href="" class="text-decoration-none col-9">Gestionar Existencias</a>
+                <span class="fa arrow" style="color: #000"></span>
+            </li>
+            <li>
+                <i class="fa fa-shopping-cart col-1" style="color: #000"></i>
+                <a href="" class="text-decoration-none col-9">Entradas</a>
+                <span class="fa arrow" style="color: #000"></span>
+            </li>
+            <li>
+                <i class="fa fa-usd col-1" style="color: #000"></i>
+                <a href="" class="text-decoration-none col-9">Ventas</a>
+                <span class="fa arrow" style="color: #000"></span>
+            </li>
+            <li>
+                <i class="fa fa-credit-card col-1" style="color: #000"></i>
+                <a href="" class="text-decoration-none col-9">Préstamos a Empleados</a>
+                <span class="fa arrow" style="color: #000"></span>
+            </li>
+            <li>
+                <i class="fa fa-money col-1" style="color: #000"></i>
+                <a href="" class="text-decoration-none col-9">Pagos a Empleados</a>
+                <span class="fa arrow" style="color: #000"></span>
+            </li>
         </ul>
     </nav>
 </aside>
