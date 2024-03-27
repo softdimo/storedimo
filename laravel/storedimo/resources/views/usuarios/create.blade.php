eee8f84f658c762cde5033c0ad47acde7b749d5c
Juangui <jgmejiaco@gmail.com>
2024-03-27 12:17:14 -0500

    campos formulario registrar personas y migración

--- laravel/storedimo/resources/views/usuarios/create.blade.php
+++ laravel/storedimo/resources/views/usuarios/create.blade.php
@@ -22,26 +22,20 @@
         {{-- ======================================================================= --}}
         {{-- ======================================================================= --}}
 
-        <div class="p-3 d-flex flex-column" style="width: 80%">
+        <div class="p-3" style="width: 80%">
             <div class="text-end">
-                <a href="#" class="nav-link text-blue">
-                    <i class="fa fa-question-circle fa-2x" aria-hidden="true" title="Ayuda" style="color: #337AB7"></i>
+                <a href="#" class="text-blue">
+                    <i class="fa fa-question-circle fa-2x" aria-hidden="false" title="Ayuda" style="color: #337AB7"></i>
                 </a>
             </div>
 
             {{-- =============================================================== --}}
             {{-- =============================================================== --}}
 
-            {!! Form::open(['method' => 'POST', 'route' => ['usuarios.store'], 'class' => 'mt-5', 'autocomplete' => 'off', 'id' => 'form_crear_usuarios']) !!}
-            @csrf
-
+            {!! Form::open(['method' => 'POST', 'route' => ['usuarios.store'], 'class' => 'mt-2', 'autocomplete' => 'off', 'id' => 'form_crear_usuarios']) !!}
+                @csrf
+            
                 @include('usuarios.fields_crear_usuarios')
-
-                <div class="row">
-                    <div class="col-12 d-flex justify-content-center">
-                        <input class="btn btn-primary rounded-pill w-25 mt-5" type="submit" value="Crear Usuario">
-                    </div>
-                </div>
             {!! Form::close() !!}
         </div>
     </div>
--- laravel/storedimo/resources/views/usuarios/fields_crear_usuarios.blade.php
+++ laravel/storedimo/resources/views/usuarios/fields_crear_usuarios.blade.php
@@ -1,164 +1,115 @@
-@extends('layouts.app')
-@section('title', 'Registrar Usuarios')
+<div id="div_crear_usuario" class="border border-2 rounded-4" style="border-color: #337AB7">
+    <h4 class="border rounded-top text-white text-center p-2" style="background-color: #337AB7">Registrar Personas (Obligatorios * )</h4>
 
-{{-- =============================================================== --}}
-{{-- =============================================================== --}}
-{{-- =============================================================== --}}
-
-@section('css')
-    {{-- <link rel="stylesheet" href="{{ asset('css/custom.css') }}"> --}}
-@stop
-
-{{-- =============================================================== --}}
-{{-- =============================================================== --}}
-{{-- =============================================================== --}}
-
-@section('content')
-    <div class="d-flex p-0">
-        <div class="p-0" style="width: 20%">
-            @include('layouts.sidebarmenu')
+    <div class="row p-5" id="div_campos_usuarios">
+        <div class="col-12 col-md-3">
+            <div class="form-group d-flex flex-column">
+                <label for="tipoPersona" class="form-label text-uppercase">Tipo persona <span class="text-danger">*</span></label>
+                {{-- {!! Form::select('txtTipoPersona', collect(['' => 'Seleccionar...'])->union($tipo_persona), null, ['class' => 'form-control', 'id' => 'tipoPersona']) !!} --}}
+                {!! Form::datetimeLocal('fecha_transaccion', null, ['class' => 'form-control', 'id' => 'tipoPersona', 'required' => 'true']) !!}
+            </div>
         </div>
 
-        {{-- ======================================================================= --}}
-        {{-- ======================================================================= --}}
+        {{-- ======================= --}}
+        
+        <div class="col-12 col-md-3">
+            <div class="form-group d-flex flex-column">
+                <label for="comprobante" class="form-label text-uppercase">Tipo de documento <span class="text-danger">*</span></label>
+                {!! Form::text('comprobante', null, ['class' => 'form-control', 'id' => 'comprobante']) !!}
+            </div>
+        </div>
 
-        <div class="p-3" style="width: 80%">
+        {{-- ======================= --}}
+        
+        <div class="col-12 col-md-3">
+            <div class="form-group d-flex flex-column">
+                <label for="id_tipo_cuenta" class="form-label text-uppercase">Número de dpcumento <span class="text-danger">*</span></label>
+                {{-- {!! Form::select('id_tipo_cuenta', collect(['' => 'Seleccionar...'])->union($tipo_cuentas), null, ['class' => 'form-control', 'id' => 'id_tipo_cuenta']) !!} --}}
+                {!! Form::text('saldo', null, ['class' => 'form-control', 'id' => 'saldo']) !!}
+            </div>
+        </div>
 
-            
-            <div id="div_crear_usuario" class="border border-1 rounded-4" style="border-color: #337AB7">
-                <h2 class="border rounded-top" style="background-color: #337AB7">Registrar Personas (Obligatorios * )</h2>
+        {{-- ======================= --}}
+        
+        <div class="col-12 col-md-3">
+            <div class="form-group d-flex flex-column">
+                <label for="id_cuenta_destino" class="form-label text-uppercase">Nombres <span class="text-danger">*</span></label>
+                {{-- {!! Form::select('id_cuenta_destino', collect(['' => 'Seleccionar...'])->union($tipo_cuentas), null, ['class' => 'form-control', 'id' => 'id_cuenta_destino']) !!} --}}
+                {!! Form::text('saldo', null, ['class' => 'form-control', 'id' => 'saldo']) !!}
+            </div>
+        </div>
 
-                <div class="row mb-1" id="div_campos_transacciones">
-                    <div class="col-12 col-md-3">
-                        <div class="form-group d-flex flex-column">
-                            <label for="fecha_transaccion" class="form-label text-uppercase">fecha transacción <span class="text-danger">*</span></label>
-                            {!! Form::datetimeLocal('fecha_transaccion', null, ['class' => 'form-control', 'id' => 'fecha_transaccion', 'required' => 'true', 'step' => '1']) !!}
-                        </div>
-                    </div>
+        {{-- ======================= --}}
         
-                    {{-- ======================= --}}
-                    
-                    <div class="col-12 col-md-3">
-                        <div class="form-group d-flex flex-column">
-                            <label for="comprobante" class="form-label text-uppercase">Comprobante</label>
-                            {!! Form::text('comprobante', null, ['class' => 'form-control', 'id' => 'comprobante']) !!}
-                        </div>
-                    </div>
-        
-                    {{-- ======================= --}}
-                    
-                    <div class="col-12 col-md-3">
-                        <div class="form-group d-flex flex-column">
-                            <label for="id_tipo_cuenta" class="form-label text-uppercase">tipo cuenta</label>
-                            {{-- {!! Form::select('id_tipo_cuenta', collect(['' => 'Seleccionar...'])->union($tipo_cuentas), null, ['class' => 'form-control', 'id' => 'id_tipo_cuenta']) !!} --}}
-                            {!! Form::text('saldo', null, ['class' => 'form-control', 'id' => 'saldo']) !!}
-                        </div>
-                    </div>
-        
-                    {{-- ======================= --}}
-                    
-                    <div class="col-12 col-md-3">
-                        <div class="form-group d-flex flex-column">
-                            <label for="id_cuenta_destino" class="form-label text-uppercase">Cuenta Destino</label>
-                            {{-- {!! Form::select('id_cuenta_destino', collect(['' => 'Seleccionar...'])->union($tipo_cuentas), null, ['class' => 'form-control', 'id' => 'id_cuenta_destino']) !!} --}}
-                            {!! Form::text('saldo', null, ['class' => 'form-control', 'id' => 'saldo']) !!}
-                        </div>
-                    </div>
-        
-                    {{-- ======================= --}}
-                    
-                    <div class="col-12 col-md-3 mt-3">
-                        <div class="form-group d-flex flex-column">
-                            <label for="id_categoria" class="form-label text-uppercase">categoria</label>
-                            {{-- {!! Form::select('id_categoria', collect(['' => 'Seleccionar...'])->union($categorias), null, ['class' => 'form-control', 'id' => 'id_categoria']) !!} --}}
-                            {!! Form::text('saldo', null, ['class' => 'form-control', 'id' => 'saldo']) !!}
-                        </div>
-                    </div>
-        
-                    {{-- ======================= --}}
-                    
-                    
-                    <div class="col-12 col-md-3 mt-3">
-                        <div class="form-group d-flex flex-column">
-                            <label for="id_sub_categoria" class="form-label text-uppercase">sub categoria</label>
-                            {{-- {!! Form::select('id_sub_categoria', collect(['' => 'Seleccionar...'])->union($sub_categorias), null, ['class' => 'form-control', 'id' => 'id_sub_categoria']) !!} --}}
-                            {!! Form::text('saldo', null, ['class' => 'form-control', 'id' => 'saldo']) !!}
-                        </div>
-                    </div>
-        
-                    {{-- ======================= --}}
-                    
-                    <div class="col-12 col-md-3 mt-3" id="">
-                        <div class="form-group d-flex flex-column">
-                            <label for="id_concepto" class="form-label text-uppercase">concepto</label>
-                            {{-- {!! Form::select('id_concepto', collect(['' => 'Seleccionar...'])->union($conceptos), null, ['class' => 'form-control', 'id' => 'id_concepto']) !!} --}}
-                            {!! Form::text('saldo', null, ['class' => 'form-control', 'id' => 'saldo']) !!}
-                        </div>
-                    </div>
-        
-                    {{-- ======================= --}}
-                    
-                    <div class="col-12 col-md-3 mt-3" id="">
-                        <div class="form-group d-flex flex-column">
-                            <label for="observaciones" class="form-label text-uppercase">observaciones</label>
-                            {!! Form::text('observaciones', null, ['class' => 'form-control', 'id' => 'observaciones']) !!}
-                        </div>
-                    </div>
-        
-                    {{-- ======================= --}}
-                    
-                    <div class="col-12 col-md-3 mt-3">
-                        <div class="form-group d-flex flex-column">
-                            <label for="ingreso" class="form-label text-uppercase">Ingreso</label>
-                            {!! Form::text('ingreso', null, ['class' => 'form-control', 'id' => 'ingreso']) !!}
-                        </div>
-                    </div>
-        
-                    {{-- ======================= --}}
-                    
-                    <div class="col-12 col-md-3 mt-3" id="">
-                        <div class="form-group d-flex flex-column">
-                            <label for="egreso" class="form-label text-uppercase">Egreso</label>
-                            {!! Form::text('egreso', null, ['class' => 'form-control', 'id' => 'egreso']) !!}
-                        </div>
-                    </div>
-        
-                    {{-- ======================= --}}
-                    
-                    <div class="col-12 col-md-3 mt-3" id="">
-                        <div class="form-group d-flex flex-column">
-                            <label for="saldo" class="form-label text-uppercase">Saldo</label>
-                            {!! Form::text('saldo', null, ['class' => 'form-control', 'id' => 'saldo']) !!}
-                        </div>
-                    </div>
-                </div> {{-- FIN div_campos_transacciones --}}
-        
-                {{-- ========================================================= --}}
-                {{-- ========================================================= --}}
-                {{-- ========================================================= --}}
-                {{-- ========================================================= --}}
-        
-                <div class="row mt-3">
-                    <div class="col-12 d-flex justify-content-center">
-                        <input class="btn btn-primary rounded-pill w-25" type="submit" value="Crear Transacción" id="btn_crear_tx">
-                    </div>
-                </div>
-            </div> {{-- FIN div_crear_transaccion --}}
+        <div class="col-12 col-md-3 mt-3">
+            <div class="form-group d-flex flex-column">
+                <label for="id_categoria" class="form-label text-uppercase">Apellidos <span class="text-danger">*</span></label>
+                {{-- {!! Form::select('id_categoria', collect(['' => 'Seleccionar...'])->union($categorias), null, ['class' => 'form-control', 'id' => 'id_categoria']) !!} --}}
+                {!! Form::text('saldo', null, ['class' => 'form-control', 'id' => 'saldo']) !!}
+            </div>
+        </div>
 
+        {{-- ======================= --}}
+        
+        
+        <div class="col-12 col-md-3 mt-3">
+            <div class="form-group d-flex flex-column">
+                <label for="id_sub_categoria" class="form-label text-uppercase">Número de teléfono</label>
+                {{-- {!! Form::select('id_sub_categoria', collect(['' => 'Seleccionar...'])->union($sub_categorias), null, ['class' => 'form-control', 'id' => 'id_sub_categoria']) !!} --}}
+                {!! Form::text('saldo', null, ['class' => 'form-control', 'id' => 'saldo']) !!}
+            </div>
+        </div>
+
+        {{-- ======================= --}}
+        
+        <div class="col-12 col-md-3 mt-3" id="">
+            <div class="form-group d-flex flex-column">
+                <label for="id_concepto" class="form-label text-uppercase">Número de Celular <span class="text-danger">*</span></label>
+                {{-- {!! Form::select('id_concepto', collect(['' => 'Seleccionar...'])->union($conceptos), null, ['class' => 'form-control', 'id' => 'id_concepto']) !!} --}}
+                {!! Form::text('saldo', null, ['class' => 'form-control', 'id' => 'saldo']) !!}
+            </div>
+        </div>
+
+        {{-- ======================= --}}
+        
+        <div class="col-12 col-md-3 mt-3" id="">
+            <div class="form-group d-flex flex-column">
+                <label for="observaciones" class="form-label text-uppercase">Correo Electrónico <span class="text-danger">*</span></label>
+                {!! Form::text('observaciones', null, ['class' => 'form-control', 'id' => 'observaciones']) !!}
+            </div>
+        </div>
+
+        {{-- ======================= --}}
+        
+        <div class="col-12 col-md-3 mt-3">
+            <div class="form-group d-flex flex-column">
+                <label for="ingreso" class="form-label text-uppercase">Género <span class="text-danger">*</span></label>
+                {!! Form::text('ingreso', null, ['class' => 'form-control', 'id' => 'ingreso']) !!}
+            </div>
+        </div>
+
+        {{-- ======================= --}}
+        
+        <div class="col-12 col-md-3 mt-3" id="">
+            <div class="form-group d-flex flex-column">
+                <label for="egreso" class="form-label text-uppercase">Dirección</label>
+                {!! Form::text('egreso', null, ['class' => 'form-control', 'id' => 'egreso']) !!}
+            </div>
+        </div>
+    </div> {{-- FIN div_campos_usuarios --}}
+
+    {{-- ========================================================= --}}
+    {{-- ========================================================= --}}
+    {{-- ========================================================= --}}
+    {{-- ========================================================= --}}
+
+    <div class="row mt-3">
+        <div class="col-6 d-flex justify-content-center">
+            <input class="btn btn-primary rounded-pill w-25" type="submit" value="Crear Usuario" id="btn_crear_usuario">
+        </div>
+
+        <div class="col-6 d-flex justify-content-center">
+            <input class="btn btn-primary rounded-pill w-25" type="submit" value="Cancelar" id="btn_cancelar">
         </div>
     </div>
-@stop
-
-{{-- =============================================================== --}}
-{{-- =============================================================== --}}
-{{-- =============================================================== --}}
-
-@section('scripts')
-    <script>
-        $( document ).ready(function() {
-            // $("#username").trigger('focus');
-        });
-    </script>
-@stop
-
-
+</div> {{-- FIN div_crear_usuario --}}
