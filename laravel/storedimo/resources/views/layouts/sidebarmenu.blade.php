028c64c4949115f500839cc6f979bf2909364764
Juangui <jgmejiaco@gmail.com>
2024-04-10 10:16:45 -0500

    implementación del collaapse del menú lateral del módulo de perssonas

--- laravel/storedimo/resources/views/layouts/sidebarmenu.blade.php
+++ laravel/storedimo/resources/views/layouts/sidebarmenu.blade.php
@@ -1,6 +1,6 @@
 <aside class="vh-100" style="border: 1px solid #e7e7e7">
     <nav class="w-100 " role="">
-        <ul class="nav d-flex flex-column justify-content-center flex-nowrap navbar-nav" id="sidebarnav">
+        <ul class="nav navbar-nav d-flex flex-column justify-content-center flex-nowrap" id="sidebarnav">
             <li class="pt-1 pb-1 d-flex justify-content-between align-items-center" style="background-color: #EEEEEE; border-bottom: 1px solid #e7e7e7">
                 <i class="fa fa-th-list text-center" style="color: #000; width: 10%"></i>
                 <a href="/home" class="nav-link active text-decoration-none text-start" style="width: 80%">Menú Principal</a>
@@ -9,24 +9,38 @@
 
             {{-- ==================================== --}}
 
-            <li class="pt-1 pb-1 d-flex justify-content-between align-items-center nav-item" style="border-bottom: 1px solid #e7e7e7">
-                <i class="fa fa-users text-center" style="color: #000; width: 10%"></i>
-                <a href="" class="text-decoration-none" style="width: 80%" id="personas" role="button" data-bs-toggle="dropdown" aria-expanded="false">Personas</a>
-                <span class="fa fa-angle-left text-center" aria-hidden="false" style="color: #000; width: 10%"></span>
+            <li class="nav-item pt-1 pb-1 d-flex flex-column" style="border-bottom: 1px solid #e7e7e7">
+                <div class="d-flex flex-row justify-content-between align-items-center" id="personas"  role="button" data-bs-toggle="collapse" data-bs-target="#ul_personas" aria-controls="ul_personas" aria-expanded="false" aria-label="Toggle navigation">
+                    <div class="col-11">
+                        <i class="fa fa-users text-center" style="color: #000; width: 10%"></i>
+                        <a href="#" class="text-decoration-none" style="width: 80%" id="">Personas</a>
+                    </div>
+                    <div class="col-1 text-center text-dark">
+                        <span class="fa fa-angle-left" aria-hidden="false" style=""></span>
+                    </div>
+                </div>
 
-                <ul class="dropdown-menu" aria-labelledby="personas">
-                    <li><a class="dropdown-item" href="{{route('usuarios.create')}}">Registrar Personas</a></li>
-                    <li><a class="dropdown-item" href="{{route('usuarios.index')}}">Listar Usuario/Empleados</a></li>
-                    <li><a class="dropdown-item" href="{{route('listar_proveedores')}}">Listar Proveedores</a></li>
-                    <li><a class="dropdown-item" href="{{route('listar_clientes')}}">Listar Clientes</a></li>
+                <ul class="nav collapse navbar-collapse ps-3" id="ul_personas">
+                    <li class="nav-item">
+                        <a class="link-underline-light" href="{{route('usuarios.create')}}">Registrar Personas</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="link-underline-light" href="{{route('usuarios.index')}}">Listar Usuario/Empleados</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="link-underline-light" href="{{route('listar_proveedores')}}">Listar Proveedores</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="link-underline-light" href="{{route('listar_clientes')}}">Listar Clientes</a>
+                    </li>
                 </ul>
             </li>
 
             {{-- ==================================== --}}
 
-            <li class="pt-1 pb-1 d-flex justify-content-between align-items-center nav-item" style="border-bottom: 1px solid #e7e7e7">
+            <li class="pt-1 pb-1 d-flex justify-content-between align-items-centerc" style="border-bottom: 1px solid #e7e7e7">
                 <i class="fa fa-database text-center" style="color: #000; width: 10%"></i>
-                <a href="" class="text-decoration-none" style="width: 80%" id="categorias" role="button" data-bs-toggle="dropdown" aria-expanded="false">Categorías</a>
+                <a href="#" class="text-decoration-none" style="width: 80%" id="categorias" role="button" data-bs-toggle="dropdown" aria-expanded="false">Categorías</a>
                 <span class="fa fa-angle-left text-center" aria-hidden="false" style="color: #000; width: 10%"></span>
                 <ul class="dropdown-menu" aria-labelledby="categorias">
                     <li><a class="dropdown-item" href="{{route('categorias.index')}}">Gestionar Categorías</a></li>
@@ -37,7 +51,7 @@
             
             <li class="pt-1 pb-1 d-flex justify-content-between align-items-center nav-item" style="border-bottom: 1px solid #e7e7e7">
                 <i class="fa fa-cubes text-center" style="color: #000; width: 10%"></i>
-                <a href="" class="text-decoration-none" style="width: 80%" id="productos" role="button" data-bs-toggle="dropdown" aria-expanded="false">Productos</a>
+                <a href="#" class="text-decoration-none" style="width: 80%" id="productos" role="button" data-bs-toggle="dropdown" aria-expanded="false">Productos</a>
                 <span class="fa fa-angle-left text-center" aria-hidden="false" style="color: #000; width: 10%"></span>
                 <ul class="dropdown-menu" aria-labelledby="productos">
                     <li><a class="dropdown-item" href="{{route('productos.create')}}">Registrar Productos</a></li>
@@ -49,7 +63,7 @@
 
             <li class="pt-1 pb-1 d-flex justify-content-between align-items-center nav-item" style="border-bottom: 1px solid #e7e7e7">
                 <i class="fa fa-check-square-o text-center" style="color: #000; width: 10%"></i>
-                <a href="" class="text-decoration-none" style="width: 80%" id="existencias" role="button" data-bs-toggle="dropdown" aria-expanded="false">Gestionar Existencias</a>
+                <a href="#" class="text-decoration-none" style="width: 80%" id="existencias" role="button" data-bs-toggle="dropdown" aria-expanded="false">Gestionar Existencias</a>
                 <span class="fa fa-angle-left text-center" aria-hidden="false" style="color: #000; width: 10%"></span>
                 <ul class="dropdown-menu" aria-labelledby="existencias">
                     <li><a class="dropdown-item" href="{{route('existencias.create')}}">Registrar Bajas</a></li>
@@ -62,7 +76,7 @@
 
             <li class="pt-1 pb-1 d-flex justify-content-between align-items-center nav-item" style="border-bottom: 1px solid #e7e7e7">
                 <i class="fa fa-shopping-cart text-center" style="color: #000; width: 10%"></i>
-                <a href="" class="text-decoration-none" style="width: 80%" id="entradas" role="button" data-bs-toggle="dropdown" aria-expanded="false">Entradas</a>
+                <a href="#" class="text-decoration-none" style="width: 80%" id="entradas" role="button" data-bs-toggle="dropdown" aria-expanded="false">Entradas</a>
                 <span class="fa fa-angle-left text-center" aria-hidden="false" style="color: #000; width: 10%"></span>
                 <ul class="dropdown-menu" aria-labelledby="entradas">
                     <li><a class="dropdown-item" href="{{route('entradas.create')}}">Registrar Entradas</a></li>
@@ -74,7 +88,7 @@
 
             <li class="pt-1 pb-1 d-flex justify-content-between align-items-center nav-item" style="border-bottom: 1px solid #e7e7e7">
                 <i class="fa fa-usd text-center" style="color: #000; width: 10%"></i>
-                <a href="" class="text-decoration-none" style="width: 80%" id="ventas" role="button" data-bs-toggle="dropdown" aria-expanded="false">Ventas</a>
+                <a href="#" class="text-decoration-none" style="width: 80%" id="ventas" role="button" data-bs-toggle="dropdown" aria-expanded="false">Ventas</a>
                 <span class="fa fa-angle-left text-center" aria-hidden="false" style="color: #000; width: 10%"></span>
                 <ul class="dropdown-menu" aria-labelledby="ventas">
                     <li><a class="dropdown-item" href="{{route('ventas.create')}}">Registrar Ventas</a></li>
@@ -87,7 +101,7 @@
 
             <li class="pt-1 pb-1 d-flex justify-content-between align-items-center nav-item" style="border-bottom: 1px solid #e7e7e7">
                 <i class="fa fa-credit-card text-center" style="color: #000; width: 10%"></i>
-                <a href="" class="text-decoration-none" style="width: 80%" id="prestamos" role="button" data-bs-toggle="dropdown" aria-expanded="false">Préstamos a Empleados</a>
+                <a href="#" class="text-decoration-none" style="width: 80%" id="prestamos" role="button" data-bs-toggle="dropdown" aria-expanded="false">Préstamos a Empleados</a>
                 <span class="fa fa-angle-left text-center" aria-hidden="false" style="color: #000; width: 10%"></span>
                 <ul class="dropdown-menu" aria-labelledby="prestamos">
                     <li><a class="dropdown-item" href="{{route('prestamo_empleados.create')}}">Registrar Préstamos</a></li>
@@ -99,7 +113,7 @@
 
             <li class="pt-1 pb-1 d-flex justify-content-between align-items-center nav-item" style="border-bottom: 1px solid #e7e7e7">
                 <i class="fa fa-money text-center" style="color: #000; width: 10%"></i>
-                <a href="" class="text-decoration-none" style="width: 80%" id="pagos" role="button" data-bs-toggle="dropdown" aria-expanded="false">Pagos a Empleados</a>
+                <a href="#" class="text-decoration-none" style="width: 80%" id="pagos" role="button" data-bs-toggle="dropdown" aria-expanded="false">Pagos a Empleados</a>
                 <span class="fa fa-angle-left text-center" aria-hidden="false" style="color: #000; width: 10%"></span>
                 <ul class="dropdown-menu" aria-labelledby="pagos">
                     <li><a class="dropdown-item" href="{{route('pago_empleados.create')}}">Registrar Pagos</a></li>
@@ -109,3 +123,9 @@
         </ul>
     </nav>
 </aside>
+
+<script>
+    // $('#personas').on('click', function () {
+    //     $('#ul_personas').removeClass('d-none');
+    // })
+</script>
