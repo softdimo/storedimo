40bebf6da50c6c54475d898ec68a3aabfdda7d41
Pablo <pablo06lara87@gmail.com>
2025-01-23 15:21:34 -0500

    Se inicia con el edit de usuario

---
+++ laravel/storedimo/resources/views/usuarios/edit.blade.php
@@ -0,0 +1,248 @@
+@extends('layouts.app')
+@section('title', 'Crear Usuarios')
+
+{{-- =============================================================== --}}
+{{-- =============================================================== --}}
+{{-- =============================================================== --}}
+
+@section('css')
+    {{-- <link rel="stylesheet" href="{{ asset('css/custom.css') }}"> --}}
+@stop
+
+{{-- =============================================================== --}}
+{{-- =============================================================== --}}
+{{-- =============================================================== --}}
+
+@section('content')
+    <div class="d-flex p-0">
+        <div class="p-0" style="width: 20%">
+            @include('layouts.sidebarmenu')
+        </div>
+
+        {{-- ======================================================================= --}}
+        {{-- ======================================================================= --}}
+
+        <div class="p-3" style="width: 80%">
+            <div class="text-end">
+                <a href="#" class="text-blue">
+                    <i class="fa fa-question-circle fa-2x" aria-hidden="false" title="Ayuda" style="color: #337AB7"></i>
+                </a>
+            </div>
+
+            {{-- =============================================================== --}}
+            {{-- =============================================================== --}}
+
+            {{-- {!! Form::open(['method' => 'PATCH', 'route' => ['hogar_gas.update', $hogarGas['id_hogar_gas']], 'class' => 'mt-5', 'autocomplete' => 'off', 'id' => 'form_edit_gas_id', 'enctype' => 'multipart/form-data', 'files' => true]) !!}
+            @csrf
+            @method('PUT') --}}
+
+            {!! Form::open(['method' => 'PATCH', 'route' => ['usuarios_update'], 'class' => 'mt-2', 'autocomplete' => 'off', 'id' => 'form_editar_usuarios']) !!}
+            @csrf
+            @method('PUT')
+            
+            @include('usuarios.fields_usuarios')
+            {!! Form::close() !!}
+        </div>
+    </div>
+@stop
+
+{{-- =============================================================== --}}
+{{-- =============================================================== --}}
+{{-- =============================================================== --}}
+
+@section('scripts')
+    <script>
+        $( document ).ready(function() {
+            $('#tipo_persona').change(function () {
+                let idTipoPersona = $('#tipo_persona').val();
+
+                if (idTipoPersona == 1) {// Empleado-fijo
+                    $('#div_fecha_contrato').removeClass('d-none');
+                    $('#fecha_contrato').attr('required');
+
+                    $('#div_rol').removeClass('d-none');
+                    $('#rol').attr('required');
+
+                    $('#div_nombre_usuario').removeClass('d-none');
+                    $('#nombre_usuario').attr('required');
+
+                    $('#div_password').removeClass('d-none');
+                    $('#password').attr('required');
+
+                    $('#div_confirmar_password').removeClass('d-none');
+                    $('#confirmar_password').attr('required');
+
+                    $('#div_nit_empresa').addClass('d-none');
+                    $('#nit_empresa').removeAttr('required');
+
+                    $('#div_nombre_empresa').addClass('d-none');
+                    $('#nombre_empresa').removeAttr('required');
+
+                    $('#div_telefono_empresa').addClass('d-none');
+                    $('#telefono_empresa').removeAttr('required');
+                }
+                // Empleado-temporal
+                else if (idTipoPersona == 2) {
+                    $('#div_fecha_contrato').addClass('d-none');
+                    $('#fecha_contrato').removeAttr('required');
+
+                    $('#div_rol').removeClass('d-none');
+                    $('#rol').attr('required');
+
+                    $('#div_nombre_usuario').removeClass('d-none');
+                    $('#nombre_usuario').attr('required');
+
+                    $('#div_password').removeClass('d-none');
+                    $('#password').attr('required');
+
+                    $('#div_confirmar_password').removeClass('d-none');
+                    $('#confirmar_password').attr('required');
+                    
+                    $('#div_nit_empresa').addClass('d-none');
+                    $('#nit_empresa').removeAttr('required');
+
+                    $('#div_nombre_empresa').addClass('d-none');
+                    $('#nombre_empresa').removeAttr('required');
+
+                    $('#div_telefono_empresa').addClass('d-none');
+                    $('#telefono_empresa').removeAttr('required');
+                }
+                // Proveedor-natural
+                else if (idTipoPersona == 3) {
+                    $('#div_fecha_contrato').addClass('d-none');
+                    $('#fecha_contrato').removeAttr('required');
+
+                    $('#div_rol').addClass('d-none');
+                    $('#rol').removeAttr('required');
+
+                    $('#div_nombre_usuario').addClass('d-none');
+                    $('#nombre_usuario').removeAttr('required');
+
+                    $('#div_password').addClass('d-none');
+                    $('#password').removeAttr('required');
+
+                    $('#div_confirmar_password').addClass('d-none');
+                    $('#confirmar_password').removeAttr('required');
+                    
+                    $('#div_nit_empresa').addClass('d-none');
+                    $('#nit_empresa').removeAttr('required');
+
+                    $('#div_nombre_empresa').addClass('d-none');
+                    $('#nombre_empresa').removeAttr('required');
+
+                    $('#div_telefono_empresa').addClass('d-none');
+                    $('#telefono_empresa').removeAttr('required');
+                }
+                // Proveedor-juridico
+                else if (idTipoPersona == 4) {
+                    $('#div_fecha_contrato').addClass('d-none');
+                    $('#fecha_contrato').removeAttr('required');
+
+                    $('#div_rol').addClass('d-none');
+                    $('#rol').removeAttr('required');
+
+                    $('#div_nombre_usuario').addClass('d-none');
+                    $('#nombre_usuario').removeAttr('required');
+
+                    $('#div_password').addClass('d-none');
+                    $('#password').removeAttr('required');
+
+                    $('#div_confirmar_password').addClass('d-none');
+                    $('#confirmar_password').removeAttr('required');
+
+                    $('#div_nit_empresa').removeClass('d-none');
+                    $('#nit_empresa').attr('required');
+
+                    $('#div_nombre_empresa').removeClass('d-none');
+                    $('#nombre_empresa').attr('required');
+
+                    $('#div_telefono_empresa').removeClass('d-none');
+                    $('#telefono_empresa').attr('required');
+                }
+                // Cliente-frecuente
+                else if (idTipoPersona == 5) {
+                    $('#div_fecha_contrato').addClass('d-none');
+                    $('#fecha_contrato').removeAttr('required');
+
+                    $('#div_rol').addClass('d-none');
+                    $('#rol').removeAttr('required');
+
+                    $('#div_nombre_usuario').addClass('d-none');
+                    $('#nombre_usuario').removeAttr('required');
+
+                    $('#div_password').addClass('d-none');
+                    $('#password').removeAttr('required');
+
+                    $('#div_confirmar_password').addClass('d-none');
+                    $('#confirmar_password').removeAttr('required');
+
+                    $('#div_nit_empresa').addClass('d-none');
+                    $('#nit_empresa').removeAttr('required');
+
+                    $('#div_nombre_empresa').addClass('d-none');
+                    $('#nombre_empresa').removeAttr('required');
+
+                    $('#div_telefono_empresa').addClass('d-none');
+                    $('#telefono_empresa').removeAttr('required');
+                }
+                //  Cliente-NO-frecuente
+                else if (idTipoPersona == 6) {
+                    $('#div_fecha_contrato').addClass('d-none');
+                    $('#fecha_contrato').removeAttr('required');
+
+                    $('#div_rol').addClass('d-none');
+                    $('#rol').removeAttr('required');
+
+                    $('#div_nombre_usuario').addClass('d-none');
+                    $('#nombre_usuario').removeAttr('required');
+
+                    $('#div_password').addClass('d-none');
+                    $('#password').removeAttr('required');
+
+                    $('#div_confirmar_password').addClass('d-none');
+                    $('#confirmar_password').removeAttr('required');
+
+                    $('#div_nit_empresa').addClass('d-none');
+                    $('#nit_empresa').removeAttr('required');
+
+                    $('#div_nombre_empresa').addClass('d-none');
+                    $('#nombre_empresa').removeAttr('required');
+
+                    $('#div_telefono_empresa').addClass('d-none');
+                    $('#telefono_empresa').removeAttr('required');
+                }
+                // Seleccionar...
+                else {
+                    $('#div_fecha_contrato').addClass('d-none');
+                    $('#fecha_contrato').removeAttr('required');
+
+                    $('#div_rol').addClass('d-none');
+                    $('#rol').removeAttr('required');
+
+                    $('#div_nombre_usuario').addClass('d-none');
+                    $('#nombre_usuario').removeAttr('required');
+
+                    $('#div_password').addClass('d-none');
+                    $('#password').removeAttr('required');
+
+                    $('#div_confirmar_password').addClass('d-none');
+                    $('#confirmar_password').removeAttr('required');
+
+                    $('#div_nit_empresa').addClass('d-none');
+                    $('#nit_empresa').removeAttr('required');
+
+                    $('#div_nombre_empresa').addClass('d-none');
+                    $('#nombre_empresa').removeAttr('required');
+
+                    $('#div_telefono_empresa').addClass('d-none');
+                    $('#telefono_empresa').removeAttr('required');
+                }
+            });
+
+            // ===================================================================================
+            // ===================================================================================
+        });
+    </script>
+@stop
+
+
---
+++ laravel/storedimo/resources/views/usuarios/fields_usuarios.blade.php
@@ -0,0 +1,78 @@
+<div class="p-0" style="border: solid 1px #337AB7; border-radius: 5px 5px 0 0;">
+    <h5 class="border rounded-top text-white text-center pt-2 pb-2 m-0" style="background-color: #337AB7">Registrar Usuarios (Obligatorios * )</h5>
+
+    <div class="row m-0 p-3" id="div_campos_usuarios">
+
+        <div class="col-12 col-md-3">
+            <div class="form-group d-flex flex-column">
+                <label for="nombre_usuario" class="form-label">Nombres <span class="text-danger">*</span></label>
+                {!! Form::text('nombre_usuario', null, ['class' => 'form-control', 'id' => 'nombre_usuario']) !!}
+            </div>
+        </div>
+
+        {{-- ======================= --}}
+
+        <div class="col-12 col-md-3">
+            <div class="form-group d-flex flex-column">
+                <label for="apellido_usuario" class="form-label">Apellidos <span class="text-danger">*</span></label>
+                {!! Form::text('apellido_usuario', null, ['class' => 'form-control', 'id' => 'apellido_usuario']) !!}
+            </div>
+        </div>
+
+        {{-- ======================= --}}
+        
+        
+        <div class="col-12 col-md-3">
+            <div class="form-group d-flex flex-column">
+                <label for="identificacion" class="form-label">Número de documento <span class="text-danger">*</span></label>
+                {!! Form::text('identificacion', null, ['class' => 'form-control', 'id' => 'identificacion']) !!}
+            </div>
+        </div>
+
+        {{-- ======================= --}}
+        
+        <div class="col-12 col-md-3" id="">
+            <div class="form-group d-flex flex-column">
+                <label for="email" class="form-label">Correo Electrónico <span class="text-danger">*</span></label>
+                {!! Form::text('email', null, ['class' => 'form-control', 'id' => 'email']) !!}
+            </div>
+        </div>
+
+        {{-- ======================= --}}
+        
+        <div class="col-12 col-md-3 mt-3">
+            <div class="form-group d-flex flex-column">
+                <label for="id_rol" class="form-label">Rol<span class="text-danger">*</span></label>
+                {!! Form::select('id_rol', collect(['' => 'Seleccionar...'])->union($roles), null, ['class' => 'form-control', 'id' => 'id_rol']) !!}
+            </div>
+        </div>
+
+        {{-- ======================= --}}
+
+
+        <div class="col-12 col-md-3 mt-3">
+            <div class="form-group d-flex flex-column">
+                <label for="id_estado" class="form-label">Estado<span class="text-danger">*</span></label>
+                {!! Form::select('id_estado', collect(['' => 'Seleccionar...'])->union($estados), 1, ['class' => 'form-control', 'id' => 'id_estado']) !!}
+            </div>
+        </div>
+
+        {{-- ========================================================= --}}
+        {{-- ========================================================= --}}
+        {{-- ========================================================= --}}
+        {{-- ========================================================= --}}
+
+        <div class="mt-5 mb-2 d-flex justify-content-center">
+            <button class="btn btn-success rounded-2 me-3" type="submit">
+                <i class="fa fa-floppy-o"></i>
+                Guardar
+                {{-- <input class="btn btn-success rounded-2 me-3" type="submit" value="Guardar" id="btn_crear_usuario"> --}}
+            </button>
+
+            <button class="btn btn-danger rounded-2" type="submit">
+                <i class="fa fa-remove"></i>
+                Cancelar
+            </button>
+        </div>
+    </div> {{-- FIN div_campos_usuarios --}}
+</div> {{-- FIN div_crear_usuario --}}
