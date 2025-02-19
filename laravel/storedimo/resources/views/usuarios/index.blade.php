1b3b889d246a5f11266fd6fbc4f6a22969313d37
Juangui <jgmejiaco@gmail.com>
2025-02-19 16:37:47 -0500

    cambio de orden del mmodal cambiar contraseña de primero y funcionan ambos

--- laravel/storedimo/resources/views/usuarios/index.blade.php
+++ laravel/storedimo/resources/views/usuarios/index.blade.php
@@ -124,7 +124,89 @@
                                             <a href="#" rel="modal:close">Cerrar</a>
                                         </div>
 
-                                        {{-- Fin modal editar usaurio --}}
+                                        {{-- ====================================================== --}}
+                                        {{-- ====================================================== --}}
+
+                                        {{-- INICIO Modal CAMBIAR CONTRASEÑA --}}
+                                        <div class="modal fade h-auto modal-gral"
+                                            id="modal_cambiar_clave_{{ $usuario->id_usuario }}" tabindex="-1"
+                                            data-bs-backdrop="static" data-bs-keyboard="false" aria-hidden="true">
+                                            <div class="modal-dialog m-0">
+                                                <div class="modal-content w-100 border-0">
+                                                    {!! Form::open([
+                                                        'method' => 'POST',
+                                                        'route' => ['cambiar_clave'],
+                                                        'class' => 'mt-2',
+                                                        'autocomplete' => 'off',
+                                                        'id' => 'formCambiarClave_' . $usuario->id_usuario,
+                                                    ]) !!}
+                                                    @csrf
+                                                    <div class="" style="border: solid 1px #337AB7;">
+                                                        <div class="rounded-top text-white text-center"
+                                                            style="background-color: #337AB7; border: solid 1px #337AB7;">
+                                                            <h5>Cambiar Contraseña de:
+                                                                {{ $usuario->identificacion . ' - ' . $usuario->nombre_usuario . ' ' . $usuario->apellido_usuario }}
+                                                            </h5>
+                                                        </div>
+
+                                                        {{ Form::hidden('id_usuario', isset($usuario) ? $usuario->id_usuario : null, ['class' => '', 'id' => 'id_usuario']) }}
+
+                                                        {{-- ====================================================== --}}
+                                                        {{-- ====================================================== --}}
+
+                                                        <div class="modal-body p-0 m-0">
+                                                            <div class="row m-0 pt-4 pb-4">
+                                                                <div class="col-12 col-md-6">
+                                                                    <div class="form-group d-flex flex-column">
+                                                                        <label for="nueva_clave" class=""
+                                                                            style="font-size: 15px">Nueva Contraseña<span
+                                                                                class="text-danger">*</span></label>
+                                                                        {{ Form::text('nueva_clave', null, ['class' => 'form-control', 'id' => 'nueva_clave_' . $usuario->id_usuario, 'placeholder' => 'Contraseña', 'required' => 'required']) }}
+                                                                    </div>
+                                                                </div>
+
+                                                                <div class="col-12 col-md-6">
+                                                                    <div class="form-group d-flex flex-column">
+                                                                        <label for="confirmar_clave" class=""
+                                                                            style="font-size: 15px">Confirmar
+                                                                            Contraseña<span
+                                                                                class="text-danger">*</span></label>
+                                                                        {{ Form::text('confirmar_clave', null, ['class' => 'form-control', 'id' => 'confirmar_clave_' . $usuario->id_usuario, 'placeholder' => 'Confirmar Contraseña', 'required' => 'required']) }}
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+
+                                                    {{-- ====================================================== --}}
+                                                    {{-- ====================================================== --}}
+
+                                                    <!-- Contenedor para el GIF -->
+                                                    <div id="loadingIndicatorEdit_{{ $usuario->id_usuario }}"
+                                                        class="loadingIndicator">
+                                                        <img src="{{ asset('imagenes/loading.gif') }}" alt="Procesando...">
+                                                    </div>
+
+                                                    {{-- ====================================================== --}}
+                                                    {{-- ====================================================== --}}
+
+                                                    <div class="d-flex justify-content-around mt-5">
+                                                        <button id="btn_editar_{{ $usuario->id_usuario }}" type="submit"
+                                                            class="btn btn-success" title="Guardar Configuración">
+                                                            <i class="fa fa-floppy-o" aria-hidden="true"> Modificar</i>
+                                                        </button>
+
+
+                                                        <button type="button" class="btn btn-secondary" title="Cancelar"
+                                                            data-bs-dismiss="modal">
+                                                            <i class="fa fa-times" aria-hidden="true"> Cancelar</i>
+                                                        </button>
+                                                    </div>
+                                                    {!! Form::close() !!}
+                                                </div>
+                                            </div>
+                                        </div>
+                                        {{-- FINAL Modal CAMBIAR CONTRASEÑA --}}
 
                                         {{-- ====================================================== --}}
                                         {{-- ====================================================== --}}
@@ -250,90 +332,6 @@
                                             </div>
                                         </div>
                                         {{-- FINAL Modal EDITAR USUARIO --}}
-
-                                        {{-- ====================================================== --}}
-                                        {{-- ====================================================== --}}
-
-                                        {{-- INICIO Modal CAMBIAR CONTRASEÑA --}}
-                                        <div class="modal fade h-auto modal-gral"
-                                            id="modal_cambiar_clave_{{ $usuario->id_usuario }}" tabindex="-1"
-                                            data-bs-backdrop="static" data-bs-keyboard="false" aria-hidden="true">
-                                            <div class="modal-dialog m-0">
-                                                <div class="modal-content w-100 border-0">
-                                                    {!! Form::open([
-                                                        'method' => 'POST',
-                                                        'route' => ['cambiar_clave'],
-                                                        'class' => 'mt-2',
-                                                        'autocomplete' => 'off',
-                                                        'id' => 'formCambiarClave_' . $usuario->id_usuario,
-                                                    ]) !!}
-                                                    @csrf
-                                                    <div class="" style="border: solid 1px #337AB7;">
-                                                        <div class="rounded-top text-white text-center"
-                                                            style="background-color: #337AB7; border: solid 1px #337AB7;">
-                                                            <h5>Cambiar Contraseña de:
-                                                                {{ $usuario->identificacion . ' - ' . $usuario->nombre_usuario . ' ' . $usuario->apellido_usuario }}
-                                                            </h5>
-                                                        </div>
-
-                                                        {{ Form::hidden('id_usuario', isset($usuario) ? $usuario->id_usuario : null, ['class' => '', 'id' => 'id_usuario']) }}
-
-                                                        {{-- ====================================================== --}}
-                                                        {{-- ====================================================== --}}
-
-                                                        <div class="modal-body p-0 m-0">
-                                                            <div class="row m-0 pt-4 pb-4">
-                                                                <div class="col-12 col-md-6">
-                                                                    <div class="form-group d-flex flex-column">
-                                                                        <label for="nueva_clave" class=""
-                                                                            style="font-size: 15px">Nueva Contraseña<span
-                                                                                class="text-danger">*</span></label>
-                                                                        {{ Form::text('nueva_clave', null, ['class' => 'form-control', 'id' => 'nueva_clave_' . $usuario->id_usuario, 'placeholder' => 'Contraseña', 'required' => 'required']) }}
-                                                                    </div>
-                                                                </div>
-
-                                                                <div class="col-12 col-md-6">
-                                                                    <div class="form-group d-flex flex-column">
-                                                                        <label for="confirmar_clave" class=""
-                                                                            style="font-size: 15px">Confirmar
-                                                                            Contraseña<span
-                                                                                class="text-danger">*</span></label>
-                                                                        {{ Form::text('confirmar_clave', null, ['class' => 'form-control', 'id' => 'confirmar_clave_' . $usuario->id_usuario, 'placeholder' => 'Confirmar Contraseña', 'required' => 'required']) }}
-                                                                    </div>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-
-                                                    {{-- ====================================================== --}}
-                                                    {{-- ====================================================== --}}
-
-                                                    <!-- Contenedor para el GIF -->
-                                                    <div id="loadingIndicatorEdit_{{ $usuario->id_usuario }}"
-                                                        class="loadingIndicator">
-                                                        <img src="{{ asset('imagenes/loading.gif') }}" alt="Procesando...">
-                                                    </div>
-
-                                                    {{-- ====================================================== --}}
-                                                    {{-- ====================================================== --}}
-
-                                                    <div class="d-flex justify-content-around mt-5">
-                                                        <button id="btn_editar_{{ $usuario->id_usuario }}" type="submit"
-                                                            class="btn btn-success" title="Guardar Configuración">
-                                                            <i class="fa fa-floppy-o" aria-hidden="true"> Modificar</i>
-                                                        </button>
-
-
-                                                        <button type="button" class="btn btn-secondary" title="Cancelar"
-                                                            data-bs-dismiss="modal">
-                                                            <i class="fa fa-times" aria-hidden="true"> Cancelar</i>
-                                                        </button>
-                                                    </div>
-                                                    {!! Form::close() !!}
-                                                </div>
-                                            </div>
-                                        </div>
-                                        {{-- FINAL Modal CAMBIAR CONTRASEÑA --}}
                                     </tr>
                                 @endforeach
                             </tbody>
