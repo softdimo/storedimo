79eec301c3fb8752230a5c88a5abb238c0b0b60c
Juangui <jgmejiaco@gmail.com>
2025-03-06 16:42:08 -0500

    funcionalidad de formulario en modal del store de productos desde entradas.create para registrar las entradas

--- laravel/storedimo/app/Http/Controllers/entradas/EntradasController.php
+++ laravel/storedimo/app/Http/Controllers/entradas/EntradasController.php
@@ -4,9 +4,26 @@ namespace App\Http\Controllers\entradas;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use GuzzleHttp\Client;
+use App\Traits\MetodosTrait;
+use Exception;
 
 class EntradasController extends Controller
 {
+    use MetodosTrait;
+    protected $baseUri;
+    protected $clientApi;
+
+    public function __construct()
+    {
+        $this->shareData();
+        $this->baseUri = env('BASE_URI');
+        $this->clientApi = new Client(['base_uri' => $this->baseUri]);
+    }
+
+    // ======================================================================
+    // ======================================================================
+
     /**
      * Display a listing of the resource.
      *
@@ -14,7 +31,25 @@ class EntradasController extends Controller
      */
     public function index()
     {
-        return view('entradas.index');
+        try {
+            if (!$this->checkDatabaseConnection()) {
+                return view('db_conexion');
+            } else {
+                $sesion = $this->validarVariablesSesion();
+
+                if (empty($sesion[0]) || is_null($sesion[0]) &&
+                    empty($sesion[1]) || is_null($sesion[1]) &&
+                    empty($sesion[2]) || is_null($sesion[2]) && !$sesion[3])
+                {
+                    return redirect()->to(route('login'));
+                } else {
+                    return view('entradas.index');
+                }
+            }
+        } catch (Exception $e) {
+            alert()->error("Exception Index Existencias!");
+            return redirect()->to(route('login'));
+        }
     }
 
     // ======================================================================
@@ -27,7 +62,25 @@ class EntradasController extends Controller
      */
     public function create()
     {
-        return view('entradas.create');
+        try {
+            if (!$this->checkDatabaseConnection()) {
+                return view('db_conexion');
+            } else {
+                $sesion = $this->validarVariablesSesion();
+
+                if (empty($sesion[0]) || is_null($sesion[0]) &&
+                    empty($sesion[1]) || is_null($sesion[1]) &&
+                    empty($sesion[2]) || is_null($sesion[2]) && !$sesion[3])
+                {
+                    return redirect()->to(route('login'));
+                } else {
+                    return view('entradas.create');
+                }
+            }
+        } catch (Exception $e) {
+            alert()->error("Exception Index Existencias!");
+            return redirect()->to(route('login'));
+        }
     }
 
     // ======================================================================
