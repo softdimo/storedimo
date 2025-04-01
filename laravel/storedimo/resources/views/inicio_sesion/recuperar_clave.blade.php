5a1d8d3d47815e0cb2b924bd29cad0b11eb2527b
pablo <pablo06lara87@gmail.com>
2025-04-01 12:50:03 -0500

    Se modifica estilo de card de recuperar contraseña

--- laravel/storedimo/resources/views/inicio_sesion/recuperar_clave.blade.php
+++ laravel/storedimo/resources/views/inicio_sesion/recuperar_clave.blade.php
@@ -2,36 +2,27 @@
 @section('title', 'Recovery Password')
 
 @section('content')
-    <div class="d-flex justify-content-center align-items-center">
-        <form class="border border-dark-subtle p-3 rounded-4" method="post" action="{{route('recuperar_clave_email')}}" autocomplete="off">
-            @csrf
-            <span class="">Recuperar Clave</span>
+<div class="d-flex justify-content-center align-items-center min-vh-100 bg-light">
+    <form class="border border-dark-subtle p-5 shadow-lg rounded-4 bg-white text-center" method="post" action="{{route('recuperar_clave_email')}}" autocomplete="off">
+        @csrf
+        <h3 class="mb-4 fw-bold text-primary">Recuperar Clave</h3>
 
-            <div class="mt-3 mb-3">
-                <input class="w-100 form-control" type="email" name="email" id="email" placeholder="Email" required>
-            </div>
+        <div class="mb-4">
+            <input class="w-100 form-control p-3" type="email" name="email" id="email" placeholder="Email" required>
+        </div>
 
-            <div class="mb-3">
-                <input class="w-100 form-control" type="text" name="identificacion" id="identificacion" placeholder="Identificación" required>
-            </div>
+        <div class="mb-4">
+            <input class="w-100 form-control p-3" type="text" name="identificacion" id="identificacion" placeholder="Identificación" required>
+        </div>
 
-            {{-- ============================================ --}}
-
-            <div class="mt-4 d-flex justify-content-end">
-                <button class="btn btn-primary" type="submit">Correo clave</button>
-            </div>
-
-            {{-- ============================================ --}}
-
-            <div class="text-left p-t-50 mt-3">
-                <span class="txt1">
-                    <a class="txt2 text-white btn btn-primary" href="{{route('login')}}">
-                        <i class="fa fa-arrow-left" aria-hidden="true"></i>&nbsp;Login
-                    </a>
-                </span>
-            </div>
-        </form>
-    </div>
+        <div class="mt-4 d-flex flex-column gap-3">
+            <button class="btn btn-primary btn-lg w-100" type="submit">Enviar Correo</button>
+            <a class="btn btn-outline-primary btn-lg w-100" href="{{route('login')}}">
+                <i class="fa fa-arrow-left" aria-hidden="true"></i> Volver al Login
+            </a>
+        </div>
+    </form>
+</div>
 @stop
 
 @section('scripts')
