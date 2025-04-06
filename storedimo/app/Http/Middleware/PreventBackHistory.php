e9e29f16651cf73c4fe1126b7c06616d3b4133d3
Juangui <jgmejiaco@gmail.com>
2025-04-06 13:54:21 -0500

    arreglo de creación QR

--- storedimo/app/Http/Middleware/PreventBackHistory.php
+++ storedimo/app/Http/Middleware/PreventBackHistory.php
@@ -16,14 +16,14 @@ class PreventBackHistory
      */
     public function handle(Request $request, Closure $next)
     {
-        $response = $next($request);
-    
-        // Cabeceras ultra-agresivas
-        return $response
-            ->header('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0, private')
-            ->header('Pragma', 'no-cache')
-            ->header('Expires', '0')
-            ->header('X-Accel-Expires', '0') // Para Nginx
-            ->header('Surrogate-Control', 'no-store');
+            $response = $next($request);
+
+            $response->headers->set('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0, private');
+            $response->headers->set('Pragma', 'no-cache');
+            $response->headers->set('Expires', '0');
+            $response->headers->set('X-Accel-Expires', '0');
+            $response->headers->set('Surrogate-Control', 'no-store');
+
+            return $response;
     }
 }
