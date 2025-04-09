f91a5abfbbbbd2c05e0d04f78dde746783d1dad6
pablo <pablo06lara87@gmail.com>
2025-04-09 12:17:48 -0500

    Se realiza modificación de tabla roles en todo el proyecto

--- storedimo/app/Models/Rol.php
+++ storedimo/app/Models/Rol.php
@@ -11,7 +11,7 @@ class Rol extends Model
 
     protected $connection = 'mysql';
     protected $table = 'roles';
-    protected $primaryKey = 'id_rol';
+    protected $primaryKey = 'id';
     protected $dates = ['deleted_at'];
     public $timestamps = true;
     protected $fillable = [
