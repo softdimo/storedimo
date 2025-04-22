94f1799269b66f5a9935710192003292d33f174c
Juangui <jgmejiaco@gmail.com>
2025-04-22 11:50:33 -0500

    se vuelve auditable el modelo Permission

--- api/app/Models/Permission.php
+++ api/app/Models/Permission.php
@@ -3,17 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 use OwenIt\Auditing\Contracts\Auditable; // Interfaz
 use OwenIt\Auditing\Auditable as AuditableTrait; // Trait
 
 class Permission extends Model implements Auditable
 {
+    use SoftDeletes;
     use AuditableTrait;
 
     protected $connection = 'mysql';
     protected $table = 'permissions';
     protected $primaryKey = 'id';
+    protected $dates = ['deleted_at'];
     public $timestamps = true;
     protected $fillable = [
         'name',
