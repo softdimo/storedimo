817ef7e6fb9c7bed206c3a5d2cef5e7699ae0f89
Juangui <jgmejiaco@gmail.com>
2025-08-28 14:40:42 -0500

    Numerar 101, Se comenta el botón PDF, solo queda excel en listar compras

--- storedimo/resources/views/entradas/index.blade.php
+++ storedimo/resources/views/entradas/index.blade.php
@@ -243,22 +243,23 @@
                 "infoEmpty": "No hay registros",
                 stripe: true,
                 bSort: true,
-                buttons: [{
-                        extend: 'pdfHtml5',
-                        text: 'PDF',
-                        className: 'btn btn-sm btn-danger',
-                        orientation: 'landscape',
-                        pageSize: 'A4', // se ajustará dinámicamente abajo
-                        title: 'Listado de Empresas',
-                        exportOptions: {
-                            columns: ':visible:not(:last-child)'
-                        },
-                        customize: function(doc) {
-                            const columnCount = $('#tbl_categorias thead th').length;
-                            doc.pageSize = 'A5';
-                            doc.defaultStyle.fontSize = 15;
-                        }
-                    },
+                buttons: [
+                    // {
+                    //     extend: 'pdfHtml5',
+                    //     text: 'PDF',
+                    //     className: 'btn btn-sm btn-danger',
+                    //     orientation: 'landscape',
+                    //     pageSize: 'A4', // se ajustará dinámicamente abajo
+                    //     title: 'Listado de Empresas',
+                    //     exportOptions: {
+                    //         columns: ':visible:not(:last-child)'
+                    //     },
+                    //     customize: function(doc) {
+                    //         const columnCount = $('#tbl_categorias thead th').length;
+                    //         doc.pageSize = 'A5';
+                    //         doc.defaultStyle.fontSize = 15;
+                    //     }
+                    // },
                     {
                         extend: 'excelHtml5',
                         text: 'Excel',
