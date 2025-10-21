d34a1317146b395be8282bcddac12e0f3ef0dcf2
Juangui <jgmejiaco@gmail.com>
2025-10-21 18:38:50 -0500

    se cambia json por query en el cuerpo de la petición de index ventas

--- storedimo/app/Http/Responsable/ventas/VentaIndex.php
+++ storedimo/app/Http/Responsable/ventas/VentaIndex.php
@@ -18,7 +18,7 @@ class VentaIndex implements Responsable
             
             // Realiza la solicitud a la API
             $peticion = $clientApi->get($baseUri . 'venta_index', [
-                'json' => [
+                'query' => [
                     'empresa_actual' => session('empresa_actual.id_empresa')
                 ]
             ]);
